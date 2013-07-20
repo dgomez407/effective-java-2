6afe733ddb723493a6469935b816c30bc234c36b
Michael Rice <me@michaelrice.com>
2013-07-19 22:13:47 -0700

    added a List<Object> example

--- src/test/java/com/michaelrice/effjava/item23/TestRawCollectionTypes.java
+++ src/test/java/com/michaelrice/effjava/item23/TestRawCollectionTypes.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 import org.junit.Test;
 
+/**
+ * This 
+ * @author mrice
+ *
+ */
 public class TestRawCollectionTypes {
 
 	/**
@@ -26,13 +31,34 @@ public class TestRawCollectionTypes {
 		//but we should hit an exception while we iterate what
 		//we think are only strings in here
 		uncheckedListOfStrings.add(new Integer(232));
-		
+
 		for (Iterator it=uncheckedListOfStrings.iterator(); it.hasNext(); ) {
 			//This should run flawlesslessly on the first iteration and 
 			//explode on the next pass
 			String testString = (String)it.next();
-		}
-		
+		}	
 	}
 
+	/**
+	 * This is quite similar to the test above, but it demonstrates what you 
+	 * might want to do if you actually want to opt out of the generic
+	 * and just store whatever kind of object in the parameterized list.
+	 *  
+	 * Notice that we don't have to deal with any warnings about our misuse
+	 * of the generics and that the for-each syntax makes a lot more sense 
+	 * here.
+	 * 
+	 */
+	@SuppressWarnings("unused")
+	@Test
+	public void testThatYouCanOptOut() {
+		List<Object> optOutListOfObjects = new ArrayList<Object>();
+
+		//both of these work, of course
+		optOutListOfObjects.add("this is cool");
+		optOutListOfObjects.add(new Integer(232));
+		//this should execute fine
+		for (Object o : optOutListOfObjects) { }
+	}
+	
 }
